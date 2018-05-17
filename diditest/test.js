0ef64393d2aa69252a810f1cd548d450dd55e587
Sherry Christian <shihongyu@didichuxing.com>
2018-05-17 19:21:18 +0800

    update didi test

---
+++ diditest/test.js
@@ -0,0 +1,10 @@
+var express =require('express');
+var app = express()
+
+app.use(express.static("dist"));
+app.use(function (req, res) {
+    res.writeHead(404,{"Content-type":"text/html;charset=UTF-8"});
+    res.end('页面没找到')
+})
+console.log('listen 20001')
+app.listen(20001)

ea5861cb525a98b11026f096714155fa933ff449
Sherry Christian <shihongyu@didichuxing.com>
2018-05-20 15:09:58 +0800

    Update

--- diditest/test.js
+++ diditest/test.js
@@ -14,6 +14,19 @@ app.post('/accredit', function (req, res) {
     // console.log()
     res.end('accredit success')
 })
+
+app.get('/gitpull', function (req, res) {
+    child.exec('git pull', function () {
+        console.log('pull')
+        res.redirect('/pullSuccess')
+    })
+})
+
+app.get('/pullSuccess', function (req, res) {
+    res.writeHead(200, {"Content-type": "text/html;charset=UTF-8"});
+    res.end('soda-b 拉取成功')
+})
+
 app.use(function (req, res) {
     res.writeHead(404,{"Content-type":"text/html;charset=UTF-8"});
     res.end('页面没找到')
